f1a70f29e1b4e37be5de854ac4b52f03d7ba07cb
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2025-12-29 21:37:56 +0100

    Implement PackedStrings container to avoid repeated allocation and  pointer chasing of strings

--- src/indexer.h
+++ src/indexer.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "utility.h"
+
 #include <filesystem>
-#include <string>
 #include <vector>
 
 namespace indexer
 {
-std::vector<std::string>
+std::vector<PackedStrings>
 scan_filesystem_parallel(const std::filesystem::path &root_path);
 } // namespace indexer
