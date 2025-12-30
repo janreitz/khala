fa80adcd90553acf217f5d69d4b2d2360a5aba4b
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2025-12-30 21:05:13 +0100

    Add functions to parse desktop files

--- src/indexer.h
+++ src/indexer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "utility.h"
+#include "actions.h"
 
 #include <filesystem>
 #include <vector>
@@ -8,4 +9,13 @@
 namespace indexer
 {
 PackedStrings scan_filesystem_parallel(const std::filesystem::path &root_path);
+
+struct DesktopApp {
+    std::string name;
+    std::string description;
+    std::string exec_command;
+    std::string desktop_file_path;
+};
+
+std::vector<DesktopApp> scan_desktop_files();
 } // namespace indexer
