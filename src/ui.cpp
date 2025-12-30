3197bedeb3319e695123a5ae384f4777e9399c5f
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2025-12-30 23:31:13 +0100

    block in input handling until first event

--- src/ui.cpp
+++ src/ui.cpp
@@ -351,8 +351,17 @@ Event process_input_events(Display *display, State &state)
 
     Event out_event = Event::NoEvent;
 
+    // Always wait for at least one event to avoid busy looping
+    XNextEvent(display, &event);
+    
+    // Process the first event
+    goto process_event;
+    
+    // Then process any additional pending events without blocking
     while (XPending(display) > 0) {
         XNextEvent(display, &event);
+        
+    process_event:
 
         if (event.type == Expose) {
             if (event.xexpose.count == 0) {
