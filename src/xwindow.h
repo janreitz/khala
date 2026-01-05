5b5ba6be349123af3b2edb46292793f3d11184a5
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2026-01-05 16:50:13 +0100

    Encapsulate remaining X code in the XWindow class

--- src/xwindow.h
+++ src/xwindow.h
@@ -8,6 +8,9 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
+#include <cairo-xlib.h>
+#include <cairo.h>
+
 #include <variant>
 #include <vector>
 
@@ -16,11 +19,14 @@ struct XWindow {
     ::Window window = 0;
     Colormap colormap = 0;
     int width = 0;
-    int height = 0;
-    int screen_height = 0;
+    unsigned int height = 0;
+    unsigned int screen_height = 0;
 
     XWindow(ui::RelScreenCoord top_left, ui::RelScreenCoord dimension);
     ~XWindow();
+
+    void resize(unsigned int height, unsigned int width);
+    cairo_surface_t* create_cairo_surface(unsigned int height, unsigned int width);
 
     // Non-copyable
     XWindow(const XWindow &) = delete;
